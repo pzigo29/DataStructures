133c362b918b8d622f0a0ed69b3145e94728dbd4
Pavol Žigo <zigo@stud.uniza.sk>
2024-03-16 22:49:21 +0100

    containsStr method

--- Semestralka/input_data.h
+++ Semestralka/input_data.h
@@ -1,7 +1,15 @@
 #pragma once
 #include <string>
 #include <vector>
+#include <fstream>
+#include <sstream>
+#include <iostream>
 #include "bus_stop.h"
+
+
+using namespace std;
+
+template <typename T>
 class InputData
 {
 	//ODSTRANENIE STOPSITE Z CSV, NAPISAT DO DOKUMENTACIE
@@ -9,7 +17,44 @@ class InputData
 public:
 	InputData(std::string file)
 		: file_(file) {};
-	std::vector<BusStop> readFromFile();
+	std::vector<std::vector<T>> readFromFile();
+
 };
+
+	template <typename T>
+	std::vector<std::vector<T>> InputData<T>::readFromFile()
+	{
+		/*vector<T> name;
+		vector<T> latitude;
+		vector<T> longtitude;
+		vector<T> code;
+		vector<T> system;
+		vector<T> town;*/
+		vector<vector<T>> busStops;
+		//busStops.insert(busStops.end(), { name, latitude, longtitude, code, system, town });
+		ifstream inputStream(file_);
+		string line;
+		getline(inputStream, line);
+		while (getline(inputStream, line))
+		{
+			vector<T> attributes;
+			stringstream sstream(line);
+			while (getline(sstream, line, ';'))
+			{
+				attributes.push_back(line);
+			}
+			/*name.push_back(attributes.at(0));
+			latitude.push_back(attributes.at(1));
+			longtitude.push_back(attributes.at(2));
+			code.push_back(attributes.at(3));
+			system.push_back(attributes.at(4));
+			town.push_back(attributes.at(5));*/
+			busStops.push_back(attributes);
+		}
+
+
+		return busStops;
+	}
+
 
 
--- Semestralka/main.cpp
+++ Semestralka/main.cpp
@@ -1,28 +1,51 @@
 #include "input_data.h"
-#include <iostream>,
+#include <iostream>
 #include <sstream>
+#include "contains_str.h"
+
 
 int main(int argc, char* argv[])
 {
 	//ODSTRANENIE STOPSITE A STOPID Z CSV, NAPISAT DO DOKUMENTACIE
-	std::vector<std::vector<BusStop>> zoznamDopravcov;
-	for (int i = 0; i < argc; i++)
+	std::vector<std::vector<std::vector<std::string>>> zoznamDopravcov;
+	for (int i = 1; i < argc; i++)
 	{
 		std::stringstream ss;
 		ss << "data/" << argv[i] << "_busstops.csv";
-		InputData input(ss.str());
-		std::vector<BusStop> zastavky = input.readFromFile();
+		InputData<std::string> input(ss.str());
+		std::vector<std::vector<std::string>> zastavky = input.readFromFile();
 		zoznamDopravcov.push_back(zastavky);
+
+		
 	}
+	auto containsStr = [](vector<string>::iterator begin, vector<string>::iterator end)
+		{
+			for (auto i = begin; i != end; ++i)
+			{
+				cout << *i << endl;
+			}
+		};
+	//containsStr(zoznamDopravcov[0][0].begin(), zoznamDopravcov[0][0].end());
+
+	ContainsStr<std::string> str;
+	for (int i = 0; i < zoznamDopravcov[0].size(); i++)
+	{
+		str.callLambda(zoznamDopravcov[0][i].begin(), zoznamDopravcov[0][i].end(), "778704");
+	}
+	
 
 	
-	//for (int i = 0; i < zoznamDopravcov.size(); i++)
-	//{
-	//	for (int j = 0; j < zoznamDopravcov[i].size(); j++)
-	//	{
-	//		zoznamDopravcov[i][j].writeConsole();
-	//	}
-	//	std::cout << "\n\n\n";
-	//}
+	/*for (int i = 0; i < zoznamDopravcov.size(); i++)
+	{
+		for (int j = 0; j < zoznamDopravcov[i].size(); j++)
+		{
+			for (int k = 0; k < zoznamDopravcov[i][j].size(); k++)
+			{
+				cout << zoznamDopravcov[i][j][k] << "\t";
+			}
+			cout << endl;
+		}
+		std::cout << "\n\n\n";
+	}*/
 	return 0;
 }
