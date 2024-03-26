53eba8cb89c8fac923f3e5da92c32f7653ae5a93
Pavol Žigo <zigo@stud.uniza.sk>
2024-03-26 14:15:33 +0100

    added filtering by csv files

--- Semestralka/contains_str.h
+++ Semestralka/contains_str.h
@@ -6,7 +6,7 @@ template <typename T1, template<typename T1> class T2, typename T3>
 class ContainsStr
 {
 private:
-
+	
 public:
 	ContainsStr() = default;
 
@@ -15,16 +15,16 @@ public:
 	void containsString(typename T2<T1>::iterator begin, typename T2<T1>::iterator end, typename const T3& string, typename T2<T1>& writeToVec,
 		std::function<bool(typename const T3&, typename const T3&)> predicate, int indexOfColumn);
 };
-
+;
 template<typename T1, template<typename T1> class T2, typename T3>
 inline void ContainsStr<T1, T2, T3>::containsString(typename T2<T1>::iterator begin, typename T2<T1>::iterator end, typename const T3& string, typename T2<T1>& writeToVec,
 	std::function<bool(typename const T3&, typename const T3&)> predicate)
 {
-	auto stringLower = string;
+	T3 stringLower = string;
 	std::transform(stringLower.begin(), stringLower.end(), stringLower.begin(), std::tolower);
 	for (auto i = begin; i != end; ++i)
 	{
-		auto iLower = *i;
+		T3 iLower = *i;
 		std::transform(iLower.begin(), iLower.end(), iLower.begin(), std::tolower);
 		if (predicate(stringLower, iLower))
 		{
@@ -37,11 +37,11 @@ template<typename T1, template<typename T1> class T2, typename T3>
 inline void ContainsStr<T1, T2, T3>::containsString(typename T2<T1>::iterator begin, typename T2<T1>::iterator end, typename const T3& string, typename T2<T1>& writeToVec,
 	std::function<bool(typename const T3&, typename const T3&)> predicate, int indexOfColumn)
 {
-	auto stringLower = string;
+	T3 stringLower = string;
 	std::transform(stringLower.begin(), stringLower.end(), stringLower.begin(), std::tolower);
 	for (auto i = begin; i != end; ++i)
 	{
-		auto iLower = i->getBlockAt(indexOfColumn);
+		T3 iLower = i->getBlockAt(indexOfColumn);
 		std::transform(iLower.begin(), iLower.end(), iLower.begin(), std::tolower);
 		if (predicate(stringLower, iLower))
 		{
--- Semestralka/main.cpp
+++ Semestralka/main.cpp
@@ -31,6 +31,8 @@ int main(int argc, char* argv[])
 		std::vector<BusStop> predikatVec;
 		std::string predikat;
 		std::string hladanyString;
+		std::string skratkaDopravcu;
+		std::vector<int> indexDopravcu;
 		int indexStlpca;
 		std::cout << "Zadaj: contains alebo startsWith (exit pre ukoncenie aplikacie)" << std::endl;
 		std::cin >> predikat;
@@ -38,22 +40,63 @@ int main(int argc, char* argv[])
 		{
 			return 0;
 		}
-		std::cout << "Parametre (oddelené medzerou) : hladanyString indexStlpca" << std::endl;
-		std::cin >> hladanyString >> indexStlpca;
+		std::cout << "Parametre (oddelené medzerou) : hladanyString indexStlpca skratkaDopravcu||indexDopravcu" << std::endl;
+		std::cin >> hladanyString >> indexStlpca >> skratkaDopravcu;
 
+		if (skratkaDopravcu == "COW" || skratkaDopravcu == "cow" || skratkaDopravcu == "0")
+		{
+			indexDopravcu.push_back(0);
+		}
+		else if (skratkaDopravcu == "KAM" || skratkaDopravcu == "kam" || skratkaDopravcu == "1")
+		{
+			indexDopravcu.push_back(1);
+		}
+		else if (skratkaDopravcu == "NAN" || skratkaDopravcu == "nan" || skratkaDopravcu == "2")
+		{
+			indexDopravcu.push_back(2);
+		}
+		else if (skratkaDopravcu == "VIC" || skratkaDopravcu == "vic" || skratkaDopravcu == "3")
+		{
+			indexDopravcu.push_back(3);
+		}
+		else if (skratkaDopravcu == "VLY" || skratkaDopravcu == "vly" || skratkaDopravcu == "4")
+		{
+			indexDopravcu.push_back(4);
+		}
+		else if (skratkaDopravcu == "WHI" || skratkaDopravcu == "whi" || skratkaDopravcu == "5")
+		{
+			indexDopravcu.push_back(5);
+		}
+		else if (skratkaDopravcu == "WIL" || skratkaDopravcu == "wil" || skratkaDopravcu == "6")
+		{
+			indexDopravcu.push_back(6);
+		}
+		else if (skratkaDopravcu == "WKT" || skratkaDopravcu == "wkt" || skratkaDopravcu == "7")
+		{
+			indexDopravcu.push_back(7);
+		}
+		else
+		{
+			if (skratkaDopravcu != "ALL" && skratkaDopravcu != "all")
+				std::cout << "Neplatný vstup, prehladavam vsetky" << std::endl;
+			for (int i = 0; i < zoznamDopravcov.size(); i++)
+			{
+				indexDopravcu.push_back(i);
+			}
+		}
 		if (predikat == "contains")
 		{
-			for (int i = 0; i < zoznamDopravcov.size(); ++i)
+			for (int i = 0; i < indexDopravcu.size(); ++i)
 			{
-				str.containsString(zoznamDopravcov[i].begin(), zoznamDopravcov[i].end(), 
+				str.containsString(zoznamDopravcov[indexDopravcu[i]].begin(), zoznamDopravcov[indexDopravcu[i]].end(),
 					hladanyString, predikatVec, contains, indexStlpca);
 			}
 		}
 		else if (predikat == "startsWith")
 		{
-			for (int i = 0; i < zoznamDopravcov.size(); ++i)
+			for (int i = 0; i < indexDopravcu.size(); ++i)
 			{
-				str.containsString(zoznamDopravcov[i].begin(), zoznamDopravcov[i].end(),
+				str.containsString(zoznamDopravcov[indexDopravcu[i]].begin(), zoznamDopravcov[indexDopravcu[i]].end(),
 					hladanyString, predikatVec, startsWith, indexStlpca);
 			}
 		}
